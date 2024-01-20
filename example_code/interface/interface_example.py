3bc09efea3be5be398b2613cb24ae99ae8c3b36c
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-01-20 15:48:05 +0300

    Finished interface example

---
+++ example_code/interface/interface_example.py
@@ -0,0 +1,6 @@
+from abc import ABCMeta
+
+class A(metaclass=ABCMeta):
+
+    def __subclasshook__(self, subclass) -> None:
+        return NotImplemented or (hasattr(subclass,"dummy_method") and callable(subclass.dummy_method))
